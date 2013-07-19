123a19387ad9b883e4a87a24f698e3a04ea4cdef
UsamaHaq <usamahaq.5533@gmail.com>
2013-07-20 02:48:53 +0500

    Home Work 3 - Part 1

---
+++ web.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var fs = require('fs');
+
+var buffer = new Buffer('Created: by Usama Haq');
+
+app.get('/', function(request, response) {
+  buffer = fs.readFileSync('./index.html', 'utf-8');
+  response.send(buffer.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
